796ae57266a5ccde231647bda6ba1c34d5922ee5
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-04-28 22:59:07 +0800

    Read file and split name and gwa of student

--- Student GWA.py
+++ Student GWA.py
@@ -3,3 +3,7 @@ with open("GWA.txt") as file:
     # Initialize lowest possible gwa and name
     gwa_value = 5.00
     highest_student = ""
+     # read file line by line
+    for line in file: 
+        # split name and gwa
+        name, gwa_str = line.strip().split("-")
