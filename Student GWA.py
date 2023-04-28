a19fb2ca773a6526a3490710601cd53a26614a5c
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-04-28 23:00:58 +0800

    Take highest GWA and student name

--- Student GWA.py
+++ Student GWA.py
@@ -3,7 +3,12 @@ with open("GWA.txt") as file:
     # Initialize lowest possible gwa and name
     gwa_value = 5.00
     highest_student = ""
-     # read file line by line
+     # Read file line by line
     for line in file: 
-        # split name and gwa
+        # Split name and gwa
         name, gwa_str = line.strip().split("-")
+        # If gwa is lower than initialized gwa, take the highest gwa and name
+        gwa = float(gwa_str)
+        if gwa < gwa_value:
+            gwa_value = gwa
+            highest_student = name
